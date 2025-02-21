0d81b7c405dc4bd2cb17eab76a3120a8427d90c2
meenakshi <meena@gmail.com>
2025-02-21 17:08:09 +0530

    committed

---
+++ redis.sh
@@ -0,0 +1,8 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+dnf install redis -y
+sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+sed -i '/protected-mode/ c protected-mode no/' /etc/redis/redis.conf
+systemctl enable redis
+systemctl start redis
